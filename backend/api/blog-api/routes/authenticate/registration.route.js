5e461468b8341753e45edace4b115119a58fbffe
Sadman Saadat <sadmansaadat99@gmail.com>
2021-07-07 12:11:24 +0600

    merge

--- backend/api/blog-api/routes/authenticate/registration.route.js
+++ backend/api/blog-api/routes/authenticate/registration.route.js
@@ -3,9 +3,23 @@ const route = express.Router()
 const checkRegistration=require('../../middlewares/authentication/registration.middleware')
 const {confirmRegistration,sendData}=require('../../controllers/AuthenticationController/registration.controller')
 
-route.get('/',(req, res)=>{
-    res.send('Registration Successfully completed')
+// route.get('/',(req, res)=>{
+//     res.send('Registration Successfully completed')
+// })
+route.post('/', (req, res) => {
+    const signedUpUser = new sendData({
+        email:req.body.email,
+        password:req.body.password,
+        name:req.body.name,
+        phone:req.body.phone
+    })
+    signedUpUser.save()
+    .then(data =>{
+        res.json(data)
+    })
+    .catch(error => {
+        res.json(error)
+    })
 })
-route.post('/',checkRegistration,sendData)
 
 module.exports=route
